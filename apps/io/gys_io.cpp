430a878728481d44c1a9da9989ce529e5d23a6d1
AlexLoup33 <a.loupoueyou@icloud.com>
2026-06-02 11:55:16 +0200

    [Update]: apps/io/main.cpp -> MPI Initialisation for subfiling

--- apps/io/gys_io.cpp
+++ apps/io/gys_io.cpp
@@ -449,7 +449,19 @@ int main(int argc, char **argv) {
    *   Main program: mini_app            *
    *
    ****************************************/
+
+  #ifdef SSF
   MPI_Init(&argc, &argv);
+  #endif __SSF__
+
+  #ifdef SUBFILING
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  if (provided != MPI_THREAD_MULTIPLE){
+    printf("provided level = %d, required level = %d\n", provided, MPI_THREAD_MULTIPLE);
+    return EXIT_FAILURE;
+  }
+  #endif __SUBFILING__
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   Kokkos::ScopeGuard scope(argc, argv);
